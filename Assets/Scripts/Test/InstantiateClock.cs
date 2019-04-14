5747d1e153e9d5c2bc53239f07ae1cba393cbe3d
Nick <teamasterln@gmail.com>
2019-04-14 14:04:43 +0300

    restart

---
+++ Assets/Scripts/Test/InstantiateClock.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateClock : MonoBehaviour
+{ public GameObject clockTile;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Instantiate(clockTile, new Vector3(0, 0, 0), Quaternion.Euler(0f, i * 15f,  0f));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
