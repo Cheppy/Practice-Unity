3d58179e0e4af2cf6aeadce0c9eacd80b1d4d7df
Nick <teamasterln@gmail.com>
2019-04-17 20:56:41 +0300

    /3d models added for the clock

---
+++ Assets/Scripts/Сlock/Clock.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Clock : MonoBehaviour
+{
+    const float
+        degreesPerHour = 30f,
+        degreesPerMinute = 6f,
+        degreesPerSecond = 6f;
+
+    public Transform hoursTransform, minutesTransform, secondsTransform;
+
+    void Awake()
+    {
+        hoursTransform.localRotation =
+            Quaternion.Euler(0f, DateTime.Now.Hour * degreesPerHour, 0f);
+    }
+}
